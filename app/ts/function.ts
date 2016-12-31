7ca9de7768872e6a905cda02e60c3cccc0dbdd0d
lulu <lu.huang@mankr.cn>
2016-12-31 22:28:58 +0800

    typescript

--- app/ts/function.ts
+++ app/ts/function.ts
@@ -24,7 +24,7 @@ var B = function () {
  */
 //指定参数name类型为string, 返回值类型为number
 function C(name:string):number {
-  return 6;
+    return 6;
 };
 
 //声明接受一个name为string类型参数,调用后返回类型为string的函数变量
@@ -32,12 +32,12 @@ function C(name:string):number {
 var D:(name:string) => string;
 //将一个符合变量类型的函数赋值给变量
 D = function ():string {
-  return "66";
+    return "66";
 };
 
 //或者简写为一行
 var D:(name:string) => string = function (name:string):string {
-  return "66";
+    return "66";
 };
 
 //以上只是为了方便对类型的理解,所添加冗余类型声明并不必须
@@ -47,17 +47,17 @@ var D:(name:string) => string = function (name:string):string {
  * 通过在参数后追加一个字符?
  */
 function E(foo:number, bar?:number, foobar?:number):number {
-  var result = foo;
+    var result = foo;
 
-  if (bar) {
-    result += bar;
-  }
+    if (bar) {
+        result += bar;
+    }
 
-  if (foobar) {
-    result += foobar;
-  }
+    if (foobar) {
+        result += foobar;
+    }
 
-  return result;
+    return result;
 };
 
 //console.log(E()); //参数不匹配
@@ -71,7 +71,7 @@ console.log(E(1, 2, 3)); //6
  * 某些场景,应为一个可选参数设置默认值
  */
 function F(foo:number, bar?:number = 0, foobar?:number = 1):number {
-  return foo + bar + foobar;
+    return foo + bar + foobar;
 };
 
 console.log(F(1)); //2
@@ -81,14 +81,14 @@ console.log(F(1)); //2
  * ...语法允许传递任意数量的参数
  */
 function G(...foo:number[]):number {
-  var result = 0;
-  var len = foo.length;
+    var result = 0;
+    var len = foo.length;
 
-  for (; len--;) {
-    result += foo[len];
-  }
+    for (; len--;) {
+        result += foo[len];
+    }
 
-  return result;
+    return result;
 };
 
 console.log(G()); // 0
@@ -106,15 +106,15 @@ function H(age:number):string;
 //function H(age:number, sex:boolean):string;
 //实现签名,兼容所有重载签名,在最后,接受一个any类型或联合类型的参数作为参数
 function H(value:(string | number)):string {
-  switch (typeof value) {
-    case "string":
-      //模板字符串
-      return `name:${value}`;
-    case "number":
-      return `age:${value}`;
-    default:
-      return "";
-  }
+    switch (typeof value) {
+        case "string":
+            //模板字符串
+            return `name:${value}`;
+        case "number":
+            return `age:${value}`;
+        default:
+            return "";
+    }
 }
 
 //console.log(H());
@@ -126,11 +126,11 @@ console.log(H(6));
  * 用一个特定的签名来创建具有同样名称,参数数量,不同返回类型的多个函数
  */
 interface I {
-  //特定重载签名
-  createI(type:"div"):HTMLDivElement;
-  createI(type:"span"):HTMLSpanElement;
-  //非特定重载签名,至少一个,包含以上特定重载签名的类型
-  createI(value:string):HTMLElement;
+    //特定重载签名
+    createI(type:"div"):HTMLDivElement;
+    createI(type:"span"):HTMLSpanElement;
+    //非特定重载签名,至少一个,包含以上特定重载签名的类型
+    createI(value:string):HTMLElement;
 }
 
 /**
@@ -144,26 +144,26 @@ interface I {
  */
 var g = 6;
 (function (global) {
-  var g = 66;
-  console.log(g); //66
-  console.log(global.g); //6
+    var g = 66;
+    console.log(g); //66
+    console.log(global.g); //6
 })(this);
 
 class J {
-  //私有属性通过IIFE实现
-  private _name:string;
+    //私有属性通过IIFE实现
+    private _name:string;
 
-  constructor() {
-    this._name = "lulu";
-  }
+    constructor() {
+        this._name = "lulu";
+    }
 
-  getName():string {
-    return this._name;
-  }
+    getName():string {
+        return this._name;
+    }
 
-  setName(val:string):void {
-    this._name = val;
-  }
+    setName(val:string):void {
+        this._name = val;
+    }
 }
 
 var j = new J();
@@ -177,29 +177,29 @@ console.log(j.getName()); //66
  * 在函数名后加<T>,表示为一个泛型函数
  */
 function K<T>(url:string, callBack:(list:T[]) => void):void {
-  var data = [];
-  //console.log(T);
+    var data = [];
+    //console.log(T);
 
-  switch (url) {
-    case "url/number":
-      data = [1, 2, 3];
-      break;
-    case "url/string":
-      data = ["66", "77"];
-      break;
-    default :
-      break;
-  }
+    switch (url) {
+        case "url/number":
+            data = [1, 2, 3];
+            break;
+        case "url/string":
+            data = ["66", "77"];
+            break;
+        default :
+            break;
+    }
 
-  callBack(data);
+    callBack(data);
 };
 
 K<number>("url/number", function (list:number[]) {
-  console.log(list);
+    console.log(list);
 });
 
 K<string>("url/string", function (list:string[]) {
-  console.log(list);
+    console.log(list);
 });
 
 /**
@@ -209,18 +209,18 @@ K<string>("url/string", function (list:string[]) {
  * tag(literals:string[], ...values:any[]):string
  */
 function L(literals, ...placeholders) {
-  var result = ""
-  var len = placeholders.length;
-  for (; len--;) {
-    result += placeholders[len] + " ";
-  }
+    var result = ""
+    var len = placeholders.length;
+    for (; len--;) {
+        result += placeholders[len] + " ";
+    }
 
-  len = literals.length;
-  for (; len--;) {
-    result += literals[len] + " ";
-  }
+    len = literals.length;
+    for (; len--;) {
+        result += literals[len] + " ";
+    }
 
-  return result;
+    return result;
 };
 
 var l = L `hello ${g} ${1 + 2} world`;
@@ -234,12 +234,12 @@ console.log(l); //3 6  world  hello
  */
 //回调函数
 var m = function () {
-  console.log("back");
+    console.log("back");
 };
 //高阶函数
 function M(callback:() => void) {
-  console.log("call");
-  callback();
+    console.log("call");
+    callback();
 };
 M(m); // 先call再back
 
@@ -249,23 +249,23 @@ M(m); // 先call再back
  */
 var _name = "77";
 class Person {
-  constructor(name:string) {
-    this._name = name;
-  }
+    constructor(name:string) {
+        this._name = name;
+    }
 
-  delay1() {
-    //不使用箭头函数,this指向setTimeout回调函数所在全局对象
-    setTimeout(function () {
-      console.log("delay1:", this._name);
-    }, 500);
-  }
+    delay1() {
+        //不使用箭头函数,this指向setTimeout回调函数所在全局对象
+        setTimeout(function () {
+            console.log("delay1:", this._name);
+        }, 100);
+    }
 
-  delay2() {
-    //使用箭头函数,this指向Person实例对象
-    setTimeout(()=> {
-      console.log("delay2:", this._name);
-    }, 500);
-  }
+    delay2() {
+        //使用箭头函数,this指向Person实例对象
+        setTimeout(()=> {
+            console.log("delay2:", this._name);
+        }, 100);
+    }
 }
 
 var p = new Person("66");
@@ -278,6 +278,81 @@ p.delay2(); //66
  * pending:初始状态
  * fulfilled:异步结果成功状态,执行then方法
  * rejected:异步结果失败状态,执行catch方法
+ * typescript并没有引入promise模式,此处引入第三方q.js,或使用ES6
  */
 //CommonJS模式引入q
 var $q = require('q');
+//异步(Async)方法,返回一个Promise对象
+function N1Async() {
+    return $q.Promise(function (resolve, reject) {
+        setTimeout(()=> {
+            try {
+                console.log("N1Async");
+                resolve({result: 1});
+            }
+            catch (e) {
+                reject();
+            }
+        }, 200);
+    });
+};
+
+function N2Aysnc() {
+    return $q.Promise(function (resolve, reject) {
+        setTimeout(()=> {
+            try {
+                console.log("N2Async");
+                resolve({result: 2});
+            }
+            catch (e) {
+                reject();
+            }
+        }, 300);
+    });
+};
+
+//串行执行3个异步方法
+N1Async().then(N2Aysnc).then(N1Async); //先N1Async再N2Async最后N1Async
+//并行执行2个异步方法
+$q.all([N1Async(), N2Aysnc()]).then((result)=> {
+    console.log(result);
+});
+
+/**
+ * 生成器
+ * 一种新型的函数,可以在执行过程中暂停多次,并随后恢复其运行
+ * 生成器为一个值序列,生成器对象为一个迭代器,可以调用next()产生结果
+ * 定义为function *XXXX(){}
+ * 意义:终止函数执行,在需要时恢复执行
+ * 暂时未支持(maybe)
+ */
+/*
+ function *O()
+ {
+ yield 1;
+ yield 2;
+ return 3;
+ }
+
+ var o = new O();
+ console.log(o.next()); //{value:1, done:false} 执行到yield 1;返回1,并终止,直到下一次next调用;
+ console.log(o.next()); //{value:2, done:false} 执行到yield 2;返回2,并终止,直到下一次next调用
+ console.log(o.next()); //{value:3, done:true} 调用next,最终才完成该函数的执行
+ console.log(o.next()); //{done:true}
+ */
+
+/**
+ * 异步函数async和await
+ * 在异步操作中被调用,使用async,await关键字指定是不阻塞程序执行还是阻塞等待异步结果
+ * 比promise模式可读性更高
+ */
+var p = N1Async();
+//异步方法
+async function P():number {
+    //阻塞等待异步结果
+    var i = await p;
+    console.log("await", i); //后输出:await Object {result:1}
+    return i.result;
+};
+
+console.log("async", P()); //先输出:async Promise
